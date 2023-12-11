138c1ddd726144cc92c9f00bad23384712fdd3c8
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-12-11 20:24:54 +0530

    adbms

---
+++ adbms/fragmentation/key.sql
@@ -0,0 +1,38 @@
+use fragmentation;
+
+CREATE TABLE Customer4(
+Customer4ID INT PRIMARY KEY,
+CustomerName VARCHAR(50),
+LastName VARCHAR(50),
+Country VARCHAR(50),
+Age int(2),
+Phone int(15)
+)
+PARTITION BY KEY ()
+partitions 3;
+INSERT INTO Customer4 (Customer4ID, CustomerName, LastName, Country, Age, Phone)
+VALUES (1, 'Shubham', 'Thakur', 'India','23','965856587'),
+(2, 'Aman ', 'Chopra', 'Australia','21','895647854'),
+(3, 'Naveen', 'Tulasi', 'Sri lanka','24','965895478'),
+(4, 'Aditya', 'Arpan', 'Austria','21','745487954'),
+(5, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','22','895326547'),
+(6, 'Alok', 'Thakur', 'India','23','965856597'),
+(7, 'Naman ', 'Chopra', 'Australia','21','895678547'),
+(8, 'Naveen', 'Tulasi', 'Sri lanka','24','965895478'),
+(9, 'Harsh', 'Arpan', 'Austria','21','745487964'),
+(10, 'Shubham. Salchichas S.A.', 'Jain', 'Spain','22','895326547'),
+(11, 'Shubham', 'Thakur', 'India','23','965856597'),
+(12, 'Aman ', 'Chopra', 'Australia','21','895648547'),
+(13, 'Naveen', 'Tulasi', 'Sri lanka','24','965875478'),
+(14, 'Kiran', 'Arpan', 'Austria','21','745487954'),
+(15, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','22','896326547'),
+(16, 'Khush', 'Thakur', 'India','23','965865897'),
+(17, 'Aman ', 'Chopra', 'Australia','21','895678547'),
+(18, 'Dev', 'Tulasi', 'Sri lanka','24','965895478'),
+(19, 'Rohan', 'Arpan', 'Austria','21','745487954'),
+(20, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','22','895632647');
+
+select partition_name,table_rows from INFORMATION_SCHEMA.partitions where table_name="Customer4";
+select * from customer4 partition(p1);
+
+SHOW TABLES;
